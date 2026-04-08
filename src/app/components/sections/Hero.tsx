349bad8ae1add86a1ae48372295a2e12eaff0d0d
DavidBugger <bwehfukdavid@gmail.com>
2026-04-08 12:09:43 +0100

    updated my resume

--- src/app/components/sections/Hero.tsx
+++ src/app/components/sections/Hero.tsx
@@ -23,8 +23,8 @@ export default function Hero() {
   const handleDownloadResume = () => {
     // Create a link element
     const link = document.createElement("a");
-    link.href = "/Akanang_David_Ignatius_Resume_Updated.pdf";
-    link.download = "Akanang_David_Ignatius_Resume_Updated.pdf";
+    link.href = "/Akanang_David_Ignatius_FullStack_Developer.pdf";
+    link.download = "Akanang_David_Ignatius_FullStack_Developer.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
